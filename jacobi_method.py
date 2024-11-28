ccfc3ff96bb713b402baa4e4a53e0d23f55da9a3
Oleksandr Bazhyn <olexandrbazhyn@gmail.com>
2024-11-28 22:57:39 +0200

    Add: file input for jacobi method

--- jacobi_method.py
+++ jacobi_method.py
@@ -1,6 +1,32 @@
 import numpy as np
 from prettytable import PrettyTable
 
+# Читання матриці з файлу
+def read_matrix_from_file(file_name):
+    try:
+        with open(file_name, 'r') as input_file:
+            matrix = []
+            for line in input_file:
+                if line.strip():
+                    row = list(map(float, line.split()))
+                    matrix.append(row)
+            return np.array(matrix)
+    except FileNotFoundError:
+        print(f"Error: Unable to open file {file_name}")
+        return None
+    
+# Виведення матриці
+def display_matrix(matrix):
+    for row in matrix:
+        print(" ".join(f"{elem:.2f}" for elem in row))
+
+def split_matrix(matrix):
+    # Витягуємо квадратну матрицю A (всі стовпці, крім останнього)
+    A = matrix[:, :-1]
+    # Витягуємо вектор стовпчик b (останній стовпець)
+    b = matrix[:, -1]
+    return A, b
+
 # Модифікована функція для методу Якобі з виведенням проміжних результатів
 def jacobi_method_verbose(A, b, tol=1e-5, max_iterations=100):
     n = len(b)
@@ -30,20 +56,30 @@ def jacobi_method_verbose(A, b, tol=1e-5, max_iterations=100):
     print(table)
     return x, max_iterations
 
-# Матриця та вектор з попередніх обчислень
-matrix = np.array([[-9, -2, -6, 1],
-                   [4, -9, 1, 0],
-                   [1, -1, 6, -1],
-                   [-5, 2, -1, -8]])
+if __name__ == "__main__":
+    file_name = "matrix_j.txt"
+    matrix = read_matrix_from_file(file_name)
 
-vector = np.array([2, -1, 6, 4])
+    if matrix is not None:
+        print("Input Matrix:")
+        display_matrix(matrix)
+        # Матриця та вектор з попередніх обчислень
+        matrix = np.array([[-9, -2, -6, 1],
+                        [4, -9, 1, 0],
+                        [1, -1, 6, -1],
+                        [-5, 2, -1, -8]])
 
-print("Розв'язок методом Якобі:")
-tol_input = input("Будь ласка, введіть бажану точність обчислень: 10 в степені -")
-tol = 10 ** (-float(tol_input))
+        vector = np.array([2, -1, 6, 4])
 
-# Виконання методу Якобі з детальним виведенням
-jacobi_solution, num_iterations = jacobi_method_verbose(matrix, vector, tol)
+        print("Розв'язок методом Якобі:")
+        tol_input = input("Будь ласка, введіть бажану точність обчислень: 10 в степені -")
+        tol = 10 ** (-float(tol_input))
 
-print("Вектор розв'язку:", jacobi_solution)
-print("Кількість ітерацій:", num_iterations)
+        # Виконання методу Якобі з детальним виведенням
+        jacobi_solution, num_iterations = jacobi_method_verbose(matrix, vector, tol)
+
+        print("Вектор розв'язку:", jacobi_solution)
+        print("Кількість ітерацій:", num_iterations)
+
+        # Перевірка правильності розв'язку
+        A, b = split_matrix(matrix)
